9cf674b13f3895ce90597e0912e097714abac190
Vitor Frasson <vbfrasson@gmail.com>
2022-01-24 14:56:55 +1000

    Lottery first commit

---
+++ contracts/Lottery.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Lottery is VRFConsumerBase, Ownable {
+    address payable[] public players;
+    uint256 public EntryFeeUSD;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
+    uint256 public fee;
+    bytes32 public keyhash;
+    address payable public recent_winner;
+    uint256 public randomness;
+
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
+        EntryFeeUSD = 50 * (10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyhash = _keyhash;
+    }
+
+    function enter() public payable {
+        // $50 minimum
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not Enough ETH");
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10; //18 decimals
+        uint256 costToEnter = (EntryFeeUSD * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
+
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Lottery currently open"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public onlyOwner {
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyhash, fee);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "You need to be Calculating the Winner to fulfill Randomnes"
+        );
+        // test to see if we get a response from cahinlink VRF
+        require(_randomness > 0, "random number not found.");
+        uint256 indexOfWinner = _randomness % players.length;
+        recent_winner = players[indexOfWinner];
+        recent_winner.transfer(address(this).balance);
+        // Reset players
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSED;
+        randomness = _randomness;
+    }
+}
